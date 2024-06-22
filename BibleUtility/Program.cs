9c2bb9fe678bbdf7b1367a9c27ed2d225bcfb4cb
Rich Todd <5732222+rgtodd@users.noreply.github.com>
2024-06-22 14:47:59 -0500

    Parse lexemes

---
+++ BibleUtility/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BibleCore;
+
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+Console.OutputEncoding = Encoding.UTF8;
+
+Console.WriteLine(Yaml.GetForms());
